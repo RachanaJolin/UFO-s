e4f330ff15ba90df27d1af004b60d1026ebe7457
RachanaJolin <55516930+RachanaJolin@users.noreply.github.com>
2020-01-17 05:31:28 -0800

    final challenge completed

--- JS/app.js
+++ JS/app.js
@@ -26,6 +26,10 @@ function buildTable(data) {
 function handleClick() {
   // Grab the datetime value from the filter
   let date = d3.select("#datetime").property("value");
+  let city = d3.select("#city").property("value");
+  let state = d3.select("#state").property("value");
+  let country = d3.select("#country").property("value");
+  let shape = d3.select("#shape").property("value");
   let filteredData = tableData;
   // Check to see if a date was entered and filter the
   // data using that date.
@@ -33,6 +37,26 @@ function handleClick() {
     // Apply `filter` to the table data to only keep the
     // rows where the `datetime` value matches the filter value
     filteredData = filteredData.filter(row => row.datetime === date);
+  }
+  if (city) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `city` value matches the filter value
+    filteredData = filteredData.filter(row => row.city === city);
+  }
+  if (state) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.state === state);
+  }
+  if (country) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `country` value matches the filter value
+    filteredData = filteredData.filter(row => row.country === country);
+  }
+  if (shape) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `shape` value matches the filter value
+    filteredData = filteredData.filter(row => row.shape === sshape);
   }
 
   buildTable(filteredData);
